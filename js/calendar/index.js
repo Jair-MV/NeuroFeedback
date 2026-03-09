65c806566b8cb03df4ce974e3f2e4365e99f718e
Jair-MV <210483879+Jair-MV@users.noreply.github.com>
2026-03-09 07:51:38 -0600

    accept incoming changes

--- js/calendar/index.js
+++ js/calendar/index.js
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-"use strict";
-
-document.addEventListener("DOMContentLoaded", function () {
-    const calendarEl = document.getElementById("calendar");
-    const calendar = new FullCalendar.Calendar(calendarEl, {
-        initialView: "dayGridMonth",
-
-        headerToolbar: {
-            start: "prev,next today",
-            center: "title",
-            end: "dayGridMonth,timeGridWeek,timeGridDay,listWeek",
-        },
-
-        buttonText: {
-            today: "Hoy",
-            dayGridMonth: "mes",
-            dayGridWeek: "semana",
-            timeGridDay: "día",
-            listWeek: "lista",
-        },
-
-        aspectRatio: 1.8,
-    });
-    calendar.render();
-});
-=======
 import { init } from "./calendar.handlers.js";
 
 const domLoadedPromise = new Promise((resolve) => {
@@ -59,4 +32,3 @@ const calendar = new FullCalendar.Calendar(calendarEl, {
 calendar.render();
 
 init(calendar);
->>>>>>> calendar
